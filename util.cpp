17ad720cbedbe65ca254c4e838c37ca91cb12ed1
faridenzx-svg <faridenzx@gmail.com>
2025-12-08 21:30:14 +0700

    Update util.cpp

--- util.cpp
+++ util.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 template<typename... Args>
 void print(Args... args) {
     (cout << ... << args);
     cout << "\n";
+}
+
+int main() {
+    string myname;
+    cout << "Enter Your Name: ";
+    cin >> myname;
+    print("My Name Is ", myname,".");
 }
