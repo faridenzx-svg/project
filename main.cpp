33b6a0c74b7c598433a2cbe179eb8fac87061fb0
faridenzx-svg <faridenzx@gmail.com>
2025-12-09 20:02:24 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,15 +12,33 @@ string pH_(double ph_number) {
     }
 }
 
+double calculator(double electric, double ammonia, string pH) {
+    if (electric <= 5 and ammonia <= 1 and pH == "Is Acidic pH") {
+        return 'Get New Rock';
+    } else {
+        return 'Wait for change new rock';
+    }
+}
  
 int main() {
+    double electric, ammonia, ph;
     string pH_Water;
-    double ph = (rand() % 140 + 1) / 10.0;
+    string calculator_;
+    // double ph = (rand() % 140 + 1) / 10.0;
+
 
     cout << "Currently pH number is: " + to_string(ph) << endl;
-    pH_Water = pH_(ph);
-    
-    cout << pH_Water << endl;
+    cout << "Currently electric number is: " + to_string(electric) << endl;
+    cout << "Currently ammonia number is: " + to_string(ammonia) << endl;
 
+
+    pH_Water = pH_(ph);
+    calculator_ = calculator(electric, ammonia, pH_Water);
+
+    if (calculator_ == "Get New Rock") {
+        cout << "Waiting For Change New Rock";
+    } else {
+        cout << calculator_;
+    }
     return 0;
 }
